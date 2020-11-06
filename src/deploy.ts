78edd27dfa6b85af653c74df0c4db2bc84bd9046
Cooper Anderson <cooper@cooperstuff.ninja>
2020-11-05 21:29:46 -0800

    Add deploying

--- src/deploy.ts
+++ src/deploy.ts
@@ -1,11 +1,31 @@
 import * as fs from "fs-extra";
+import { exec } from "child_process";
 
 type Config = {
-	path: string,
+	destination: string,
 	ids: {computer: number[], tablet: number[], turtle: number[]}
 };
 
-const config: Config = fs.readJsonSync("lua/config.json");
+function setupID(type: string, id: number) {
+	fs.copySync(`dist/${type}`, `dist/final/${id}`);
+}
 
-console.log(config);
+const config: Config = fs.readJsonSync("lua/config.json");
+if (config.destination === "") {
+	console.warn("Please set a destination in the `lua/config.json` file.")
+	process.exit();
+}
+
+fs.ensureDir("dist/final");
+for (let id of config.ids.computer) setupID("computer", id);
+for (let id of config.ids.tablet) setupID("tablet", id);
+for (let id of config.ids.turtle) setupID("turtle", id);
+
+let final = fs.readdirSync("dist/final");
+for (let id of final) {
+	console.log("Sending id: " + id);
+	exec(`scp -r dist/final/${id} ${config.destination}`);
+}
+
+fs.rmdirSync("dist/final", {recursive: true});
 
