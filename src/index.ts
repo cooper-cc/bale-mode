40339a9f9d7baefee86b9c3672f2835eb8b255fe
Cooper Anderson <cooper@cooperstuff.ninja>
2020-11-05 20:31:38 -0800

    Add basic lua compilation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-export default class Main {
-	constructor() {
-		console.log("beep");
-	}
-}
+import * as fs from "fs-extra";
+
+fs.rmdirSync("./dist", {recursive: true});
+fs.mkdirSync("./dist");
+
+fs.copySync("./lua/common", "./dist/computer");
+fs.copySync("./lua/common", "./dist/tablet");
+fs.copySync("./lua/common", "./dist/turtle");
+
+fs.copySync("./lua/computer", "./dist/computer");
+fs.copySync("./lua/tablet", "./dist/tablet");
+fs.copySync("./lua/turtle", "./dist/turtle");
+
