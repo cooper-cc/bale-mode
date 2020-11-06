2eb2e0ceb3d0e872bc4153722217a0eb9f8a980e
Cooper Anderson <cooper@cooperstuff.ninja>
2020-11-05 19:29:57 -0800

    Initial files

---
+++ lua/turtle/lib/kernal.lua
@@ -0,0 +1,13 @@
+turtle.prototype = {
+	forward = turtle.forward,
+	back = turtle.back,
+	turnLeft = turtle.turnLeft,
+	turnRight = turtle.turnRight,
+	up = turtle.up,
+	down = turtle.down
+}
+
+turtle.forward = function()
+
+end
+
